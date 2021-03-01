3f1441c4e310bff06d5d6b668647c1e46cbe5c97
Holmfred <fredrik1304@live.no>
2021-03-01 22:46:33 +0100

    Update 1.0

---
+++ js/script.js
@@ -0,0 +1,63 @@
+console.log ("Hello")
+var name;
+name ="mary";
+
+//question 1
+var integer = 3;
+var decimal = 4.7;
+
+//Question 2
+var daysOfTheWeek = "Monday" + "-" + "Tuesday" + "-" + "Wednesday" + "-" + "Thursday" + "-" + "Friday";
+console.log(daysOfTheWeek)
+
+//Question 3
+var additon = 5+5;
+var subtraction = 3-1;
+var multiplication = 3*3;
+var division = 10/5;
+
+//Question 4
+var loggedIn = false
+
+//Question 5
+var sentence = "The frogs are angry"
+console.log(sentence.length)
+
+//Question 6
+var timeElapsed;
+var totalPrice;
+
+//Question 7
+var number1 = "53";
+var number2 = "44.6";
+
+var convertNumber1 = parseInt(number1);
+var convertNumber2 = parseFloat(number2);
+
+//Question 8
+var pet = "donkey";
+var age = 89;
+var discountApplied = true;
+var timeLeft;
+
+var typeOfPet = typeof pet;
+console.log(typeOfPet);
+
+var typeOfAge = typeof age;
+console.log (typeOfAge);
+
+var typeOfDiscoundApplied = typeof discountApplied;
+console.log(typeOfDiscoundApplied);
+
+var typeOfTimeleft = typeof timeLeft;
+console.log(typeOfTimeLeft);
+
+//Question 9
+var headingText = "Updating heading";
+
+var heading = document.querySelector("h1");
+heading.innerHTML = headingText;
+
+//Question 10
+var body = document.querySelector("body");
+body.style.backgroundColor = "lightskyblue"
